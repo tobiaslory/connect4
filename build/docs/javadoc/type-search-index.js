bde0429740540a7e36b260866e2ef125de194c16
Tobias Lory <tobias.lory@opremic.solutions>
2020-02-14 11:11:39 +0100

    changed doc location for usage with github pages

--- build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.connect4","l":"GameBoard"},{"p":"org.connect4","l":"GameBoard.InvalidMoveException"},{"p":"org.connect4","l":"InvalidStateException"}]
---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,4 @@
+typeSearchIndex = [{"l": "All Classes", "url": "allclasses-index.html"}, {
+    "p": "org.connect4",
+    "l": "GameBoard"
+}, {"p": "org.connect4", "l": "GameBoard.InvalidMoveException"}, {"p": "org.connect4", "l": "InvalidStateException"}];
